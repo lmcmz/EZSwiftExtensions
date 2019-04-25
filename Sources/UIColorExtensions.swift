c1d70b47711933560183ed3dd83fd4a24fe27a4d
lmcmz <lmcmze@gmail.com>
2019-04-26 00:31:27 +1000

    Update to Swift5

--- Sources/UIColorExtensions.swift
+++ Sources/UIColorExtensions.swift
@@ -16,19 +16,6 @@ extension UIColor {
         self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
     }
 
-    /// EZSE: init method with hex string and alpha(default: 1)
-    public convenience init?(hexString: String, alpha: CGFloat = 1.0) {
-        var formatted = hexString.replacingOccurrences(of: "0x", with: "")
-        formatted = formatted.replacingOccurrences(of: "#", with: "")
-        if let hex = Int(formatted, radix: 16) {
-          let red = CGFloat(CGFloat((hex & 0xFF0000) >> 16)/255.0)
-          let green = CGFloat(CGFloat((hex & 0x00FF00) >> 8)/255.0)
-          let blue = CGFloat(CGFloat((hex & 0x0000FF) >> 0)/255.0)
-          self.init(red: red, green: green, blue: blue, alpha: alpha)        } else {
-            return nil
-        }
-    }
-
     /// EZSE: init method from Gray value and alpha(default:1)
     public convenience init(gray: CGFloat, alpha: CGFloat = 1) {
         self.init(red: gray/255, green: gray/255, blue: gray/255, alpha: alpha)
